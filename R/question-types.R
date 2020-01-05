a77c5f577a5d9534c9d5a8b9d43d9252703b4adb
davidkep <david@rotinaj.org>
2020-01-05 10:36:41 -0800

    export new question types

--- R/question-types.R
+++ R/question-types.R
@@ -3,11 +3,13 @@
 #' Wrapper around [learnr::question] with sensible defaults for multiple choice questions.
 #' @param ...,type,allow_retry,random_answer_order see [learnr::question] for these parameters
 #' @param correct a vector of strings to be displayed for correct answers. One of these strings is chosen at random.
+#' @importFrom learnr question
+#' @export
 mcquestion <- function (..., type = 'learnr_checkbox', allow_retry = TRUE, random_answer_order = TRUE,
                         correct = c('Perfect!', 'Nice!', 'Well done!', 'Good job!', 'Great!', 'Correct!', 'Exactly!')) {
-  learnr::question(...,
-                   type = 'learnr_checkbox',
-                   allow_retry = TRUE,
-                   correct = sample(correct, 1),
-                   random_answer_order = TRUE)
+  question(...,
+           type = 'learnr_checkbox',
+           allow_retry = TRUE,
+           correct = sample(correct, 1),
+           random_answer_order = TRUE)
 }
