4ee3d58fcb1f364e4eeb88a7be786a417b14d395
davidkep <david@rotinaj.org>
2020-08-14 11:02:46 -0700

    make unix package optional and don't use the safe evaluator on macOS

--- R/eval_safe.R
+++ R/eval_safe.R
@@ -6,14 +6,18 @@
 #' @param max_data maximum size of the data memory in MiB.
 #' @param priority priority of the evaluating process.
 #' @param rlimits specify custom rlimitis.
+#' @param force_safe_unix force the use of the [unix::eval_safe()] function for evaluating user code.
+#'  The `unix` package is not available on Windows and is not used by default on macOS.
 #' @param allow_env vector of regular expressions of allowed environment variables (i.e., the matching variables will
 #'                  be retained).
 #' @param default_allow_env vector of regular expressions of environment variables which should be allowed by default.
 #'                          Only change if you know what you are doing, as most of them are fundamental to the R process
 #'                          to work.
 #' @export
-enable_safe_eval <- function (max_fsize = 1L, envir = parent.frame(), max_address_space = 2048L, max_data = 256L,
+#' @importFrom rlang warn
+enable_safe_eval <- function (max_fsize = 1L, envir = new.env(), max_address_space = 2048L, max_data = 256L,
                               priority = 20L, rlimits, allow_env,
+                              force_safe_unix = FALSE,
                               default_allow_env = c('^SHELL$', '^USER$', '^LANG$', '^LC_CTYPE$', '^HOME$',
                                                     '^DYLD_FALLBACK_LIBRARY_PATH$', '^R_.*')) {
   if (missing(rlimits)) {
@@ -26,17 +30,22 @@ enable_safe_eval <- function (max_fsize = 1L, envir = parent.frame(), max_addres
     c(allow_env, default_allow_env)
   }
 
-  options(tutorial.exercise.evaluator = get_safe_evaluator(priority, envir = envir, rlimits, allow_env))
+  if (isTRUE(force_safe_unix) || (Sys.info()[['sysname']] != 'Darwin' && isNamespaceLoaded('unix'))) {
+    options(tutorial.exercise.evaluator = get_safe_evaluator_unix(priority, envir = envir, rlimits, allow_env))
+  } else {
+    warn("Using an unsafe evaluator!")
+    options(tutorial.exercise.evaluator = get_safe_evaluator_other(envir = envir))
+  }
+
 }
 
-#' Get a function to safely evaluate user code.
-#'
+## Get a function to safely evaluate user code.
+##
 #' @param priority,rlimits,allow_env see [enable_safe_eval] for details.
 #' @return a `function(expr, timeout)` which evaluates the given expression `expr` safely in at most `timeout` seconds.
-#'
-#' @importFrom unix eval_safe
 #' @importFrom shiny div
-get_safe_evaluator <- function (priority, envir, rlimits, allow_env) {
+#' @importFrom rlang warn abort with_abort
+get_safe_evaluator_unix <- function (priority, envir, rlimits, allow_env) {
   # Clear the environment before evaluating the call.
   outer_envir <- new.env()
   outer_envir$priority <- priority
@@ -50,7 +59,7 @@ get_safe_evaluator <- function (priority, envir, rlimits, allow_env) {
 
     Sys.unsetenv(envvars[to_remove])
   }
-  outer_envir$envir <- envir
+  outer_envir$eval_envir <- envir
 
   return(function (expr, timeout) {
     result <- NULL
@@ -63,23 +72,76 @@ get_safe_evaluator <- function (priority, envir, rlimits, allow_env) {
 
     return(list(
       start = function() {
-        tryCatch(
-          result <<- evalq(eval_safe({
+        result <<- tryCatch(
+          rlang::with_abort(evalq(unix::eval_safe({
             clear_env()
             tryCatch({
               .setup <- parse(text = trimws(exercise$options$exercise.checker, whitespace = '"'))
-              add_to_envir <- eval(.setup, envir)
+              add_to_envir <- eval(.setup, eval_envir)
               for (newvar in names(add_to_envir)) {
-                assign(newvar, add_to_envir[[newvar]], envir)
+                assign(newvar, add_to_envir[[newvar]], envir = eval_envir)
               }
               rm(.setup, add_to_envir)
-            }, error = function (e) {})
+            }, error = function (e) {
+              rlang::warn(paste("Cannot evaluate setup chunk: ", e))
+            }, warning = function (w) {})
             eval(expr)
-          }, timeout = timeout, priority = priority, rlimits = rlimits), envir = outer_envir),
+          }, timeout = timeout, priority = priority, rlimits = rlimits), envir = outer_envir)),
           error = function (e) {
-            result <<- list(feedback = NULL,
-                            error_message = as.character(e),
-                            html_output = div(class = 'alert alert-danger', role = 'alert', as.character(e)))
+            print(e)
+            return(list(feedback = NULL,
+                        error_message = as.character(e),
+                        html_output = div(class = 'alert alert-danger', role = 'alert', as.character(e))))
+          })
+      },
+      completed = function() {
+        return(TRUE)
+      },
+      result = function() {
+        return(result)
+      }
+    ))
+  })
+}
+
+## Get a function to safely evaluate user code.
+##
+#' @importFrom shiny div
+#' @importFrom rlang warn abort with_abort
+get_safe_evaluator_other <- function (envir) {
+  # Clear the environment before evaluating the call.
+  outer_envir <- new.env()
+  outer_envir$eval_envir <- envir
+
+  return(function (expr, timeout) {
+    result <- NULL
+    outer_envir$expr <- substitute(expr)
+    outer_envir$timeout <- timeout
+
+    # Pull learnr stuff into outer environment
+    outer_envir$exercise <- parent.frame()$exercise
+    outer_envir$evaluate_exercise <- learnr:::evaluate_exercise
+
+    return(list(
+      start = function() {
+        result <<- tryCatch(
+          rlang::with_abort(evalq({
+            tryCatch({
+              .setup <- parse(text = trimws(exercise$options$exercise.checker, whitespace = '"'))
+              add_to_envir <- eval(.setup, eval_envir)
+              for (newvar in names(add_to_envir)) {
+                assign(newvar, add_to_envir[[newvar]], envir = eval_envir)
+              }
+              rm(newvar, .setup, add_to_envir)
+            }, error = function (e) {
+              rlang::warn(paste("Cannot evaluate setup chunk: ", e))
+            }, warning = function (w) {})
+            eval(expr)
+          }, envir = outer_envir)),
+          error = function (e) {
+            return(list(feedback = NULL,
+                        error_message = as.character(e),
+                        html_output = div(class = 'alert alert-danger', role = 'alert', as.character(e))))
           })
       },
       completed = function() {
